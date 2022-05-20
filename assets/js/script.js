20bdfad3fb23f57a6696c47aa2ec85d82c9a1d40
Charlie <charlottevfarmer@gmail.com>
2022-05-20 11:38:35 +0000

    Add code for division game from division challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
 
-
     runGame("addition");
 })
 
@@ -52,7 +51,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division") {
+	    displayDivisionQuestion(num1, num2);
+	} else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -104,7 +105,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, 'multiply'];
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
-    } else {
+    } else if (operator === "/") {
+	    return [operand1 / operand2, "division"];
+	} else {
             alert(`Unimplemented operator ${operator}`);
             throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -161,4 +164,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     // the standard computer symbol for multiplication is *, but x
     // is a bit nicer/non-tech-user friendly
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    
+    document.getElementById("operand1").textContent = (operand1 * operand2);
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/"
 }
